13232b5427cd2ee7fa60eb38a4e38a9bc1a35dc7
Joan938 <martinezibarrondojoan@gmail.com>
2025-05-13 13:08:17 -0400

    Making task 1

--- factorial.py
+++ factorial.py
@@ -5,23 +5,23 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  # You need to decrement n to avoid an infinite loop
     return result
 
-# Check if a number is provided
-if len(sys.argv) != 2:
-    print("Usage: ./script.py <number>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-    if number < 0:
-        print("Factorial is not defined for negative numbers.")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <non-negative integer>")
         sys.exit(1)
-except ValueError:
-    print("Please provide a valid integer.")
-    sys.exit(1)
 
-f = factorial(number)
-print(f)
+    try:
+        n = int(sys.argv[1])
+        if n < 0:
+            print("Error: Factorial is not defined for negative numbers.")
+            sys.exit(1)
+    except ValueError:
+        print("Error: Please enter a valid integer.")
+        sys.exit(1)
+
+    f = factorial(n)
+    print(f)
 
